a629b5cf5bb0ccb043368d6d7592aea98229ac13
Tom Roper <Tom@MacBook-Pro.local>
2016-06-23 10:01:56 -0700

    initial React page setup. Used boilerplate from previous project

---
+++ src/components/home.jsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Router, Route, browserHistory, Redirect} from 'react-router';
+
+export default class Home extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+  
+    }
+  }
+
+
+
+
+
+  render() {
+  
+    return (
+      <div>
+        <p>This is the react home component</p>
+      </div>);
+
+
+  } //render close
+} //App close
